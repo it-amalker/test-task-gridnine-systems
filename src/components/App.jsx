7a1e62f86cdf080c767424a4926891329ce116fb
amalker <it.amalker@yandex.ru>
2020-07-08 13:35:44 +0300

    add lang state, add Language component to render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import flightsResponse from '../data/flights.json';
 import FlightCards from './FlightCards';
 import Sort from './Sort';
+import Languages from './Languages';
 
 const App = () => {
   const [flights, setFlights] = useState(flightsResponse.result.flights);
+  const [activeLang, setActiveLang] = useState('ru');
+
   const [filters, setFilters] = useState({
     noTransfers: false,
     minPrice: null,
@@ -12,10 +15,23 @@ const App = () => {
   });
 
   return (
-    <div className="app-container">
-      <Sort flights={flights} setFlights={setFlights} setFilters={setFilters} />
-      <FlightCards flights={flights} filters={filters} />
-    </div>
+    <>
+      <div>
+        <Languages activeLang={activeLang} setActiveLang={setActiveLang} />
+      </div>
+      <div className="app-container">
+        <Sort
+          flights={flights}
+          setFlights={setFlights}
+          setFilters={setFilters}
+        />
+        <FlightCards
+          flights={flights}
+          filters={filters}
+          activeLang={activeLang}
+        />
+      </div>
+    </>
   );
 };
 
