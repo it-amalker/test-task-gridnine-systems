dedf0af3485fbe2a6e36b2ab251ebaa99df0f5c0
amalker <it.amalker@yandex.ru>
2020-07-08 13:29:24 +0300

    add locales init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './locales/init/i18n';
 
 import './styles/style.css';
 import 'normalize.css';
